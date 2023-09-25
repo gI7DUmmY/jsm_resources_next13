fcbe2f89a16de89e5f79730bd135f40e4286f985
Cedric Rollo <cedric.rollo@gmail.com>
2023-09-25 21:49:01 +0200

    Header props

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,9 @@
 interface Props {
-  title: string
   query?: string
   category?: string
 }
 
-const Header = ({ title, query, category }: Props) => {
+const Header = ({ query, category }: Props) => {
   if (query && category) {
     return (
       <h1 className='heading3 self-start text-white-800'>
